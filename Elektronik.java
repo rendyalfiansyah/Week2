2a0afc7e63984569a8f2d8a81672bb059f1b0d26
rendyalfiansyah <rendyalfiasyah356@gmail.com>
2026-05-06 10:26:24 +0700

    add konstruktor

--- Elektronik.java
+++ Elektronik.java
@@ -3,8 +3,12 @@ public class Elektronik extends produk {
     private int garansiTahun;
     private static final double PPN = 0.11;
 
-    
-    public Elektronik (String nama, double harga, int stok, String kategori, int garansi){
+public Elektronik (String nama, String merek, double harga, int stok, int garansiTahun {
+    super(nama, harga, stok);
+    this.merek = merek;
+    this. garansiTahun = garansiTahun;
+}
+
       super(nama, harga, stok, kategori);
       this.garansi = garansi;
     }
